a8f36bde69176c14557d6fc0610ab219dc11d58e
Khokon M <khokonads@gmail.com>
2025-03-07 21:45:29 +0600

    Removed type=module from package.json

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 
-import { execSync } from "child_process";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
 const [, , projectName] = process.argv;
 
